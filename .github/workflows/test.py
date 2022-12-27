af47e43aa6d7999f329e097eed0f09b0083dfe59
GrootCHENN <112602646+GrootCHENN@users.noreply.github.com>
2022-12-27 16:27:00 +0800

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,8 +1,11 @@
+import requests
 
 def hello_world():
   with open("./demo.txt","r") as f:
     txt = f.read()
     print(txt)
+  data = requests.get("http://43.142.76.49:8989/kaoyanbisheng").text
+  print(data)
  
 if __name__ == "__main__":
   hello_world()
